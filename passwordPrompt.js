84791057dcced39b2799aa23cd0761973c479088
unknown <zedlavhtide@gamil.com>
2024-10-03 22:02:15 +0800

    commit

---
+++ passwordPrompt.js
@@ -0,0 +1,13 @@
+// correct password
+let correctPassword = "KoreanaCutie18";
+
+// Prompt the user to enter their password
+let password ="KoreanaCutie18";
+
+// Use a do-while loop  user enters the correct password
+do {if (password === correctPassword) 
+    {console.log("Access granted.");} 
+    else 
+    {console.log("Incorrect, try again.");
+    password ="FilipinaCutie18";}} 
+    while (password !== correctPassword);
